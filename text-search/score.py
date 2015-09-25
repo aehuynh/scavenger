097225308f475caadb7e3c28d421996d9f31d532
aehuynh <aehuynh@bu.edu>
2015-09-25 11:29:35 -0400

    Refactored to pass in all the required parameters in the constructor instead of passing in an index reader object

--- text-search/score.py
+++ text-search/score.py
@@ -1,41 +1,47 @@
 class Scorer(object):
-    def __init__(self, index):
-        self.index = index
+
+    def __init__(self, doc_count, doc_freqs, doc_lengths, avgdl):
+        self.doc_count = doc_count
+        self.doc_freqs = doc_freqs
+        self.doc_lengths = doc_lengths
+        self.avgdl = avgdl
 
     def score(self, doc_word):
         raise NotImplementedError
 
-    def idf(self, doc_word):
-        total_doc_count = self.index.total_doc_count
-        doc_freq = self.index.doc_freq(doc_word.word)
+    def tf(self, doc_word):
+        return doc_word.count / self.doc_lengths[doc_word.document_id]
 
-        return log(total_doc_count / (doc_freq + 1.0)) + 1.0
+    def idf(self, doc_word):
+        doc_freq = self.doc_freqs[doc_word.word]
+
+        return log(self.doc_count / (doc_freq + 1.0)) + 1.0
 
 class BM25(Scorer):
-    def __init__(self, index, k=1.2, b=0.75):
-        super().__init__(index)
+
+    def __init__(self, b=0.75, k=1.2, **kwargs):
+        super().__init__(**kwargs)
         self.k = k
         self.b = b
 
     def score(self, doc_word):
         B = self.b
         K = self.k
-        tf = log(1 + doc_word.term_freq)
+        tf = tf(doc_word)
         idf = self.idf(doc_word)
-        doc_word_count = self.index.doc_word_count(doc_word.document_id)
-        avgdl = self.index.avgdl
+        doc_length = self.doc_lengths[doc_word.document_id]
+        avgdl = self.avgdl
 
-        return idf * tf * (K + 1) /
-            (tf + K +*(1 - B  + B * doc_word_count / avgdl))
+        return idf * tf * (K+1) / (tf + K * (1 - B  + B * doc_length / avgdl))
 
 class TF_IDF(Scorer):
-    def __init__(self, index):
-        super().__init__(index)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def score(self, doc_word):
-        # Uses term frequency in proportion to the document word count
-        tf = doc_word.term_freq / self.doc_word_count(doc_word.document_id)
+        # Term frequency in proportion to the total document word count
+        tf = tf(doc_word)
         idf = self.idf(doc_word)
 
         return tf * idf
-
